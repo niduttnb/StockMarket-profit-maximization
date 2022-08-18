9add2d5501a44d220b7330206874810178d55081
Nidutt Bhuptani <niduttnb@gmail.com>
2022-08-17 23:03:28 -0400

    Update dp_one.py

--- dp_one.py
+++ dp_one.py
@@ -1,8 +1,12 @@
 def dp_one(prices):
-  ans = max_val = 0
-  i=1
-  while(i<len(prices)):
-      max_val = max(max_val + prices[i] - prices[i-1],0)
-      ans = max(max_val, ans)
-      i+=1
-  return ans 
+
+    profit = [0 for x in range(len(prices))] # profit
+    min_value = prices[0] # minimum values of the stocks on the left
+    i = 1     
+    
+    while(i < len(prices) + 1):
+        min_value = min(min_value, prices[i - 1])
+        profit.append(max(profit[i - 1], prices[i - 1] - min_value))
+        i += 1
+        
+    return profit[-1]
