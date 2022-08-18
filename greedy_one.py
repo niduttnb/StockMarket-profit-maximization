a85db7b477545c9fc8ffd39a6ee9d948b10489ef
Nidutt Bhuptani <niduttnb@gmail.com>
2022-08-17 23:04:17 -0400

    Update greedy_one.py

--- greedy_one.py
+++ greedy_one.py
@@ -1,10 +1,12 @@
-
 def greedy_one(prices):
+
   l = float('inf')
-  result = 0
-  i=0
-  while i<len(prices):
+  profit = 0
+  i = 0
+  
+  while i < len(prices):
     l = min(l, prices[i])
-    result = max(result, prices[i] - l)
-    i+=1
-  return result
+    profit = max(profit, prices[i] - l)
+    i += 1
+    
+  return profit
